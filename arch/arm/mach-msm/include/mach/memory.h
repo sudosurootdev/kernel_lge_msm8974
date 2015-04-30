55e42bf00ea8b9ec1ec62434098876a5fdd39ad1
lawn <bloom.all.over.12@gmail.com>
2015-04-30 10:25:48 +0900

    L01F : Add Kexec Hardboot

--- arch/arm/mach-msm/include/mach/memory.h
+++ arch/arm/mach-msm/include/mach/memory.h
@@ -37,7 +37,8 @@
 		defined(CONFIG_MACH_MSM8974_G2_KT) || \
 		defined(CONFIG_MACH_MSM8974_G2_SPR) || \
 		defined(CONFIG_MACH_MSM8974_G2_VZW) || \
-		defined(CONFIG_MACH_MSM8974_G2_KDDI)
+		defined(CONFIG_MACH_MSM8974_G2_KDDI) || \
+		defined(CONFIG_MACH_MSM8974_G2_DCM)
 #define KEXEC_HB_PAGE_ADDR		UL(0x07b00000)
 #else
 #error "Adress for kexec hardboot page not defined"
